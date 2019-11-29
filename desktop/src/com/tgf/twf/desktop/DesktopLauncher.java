01a84034a75401ceb0772b1aefcf1986ef7f0d62
Fernand Sieber <sieberf@amazon.com>
2019-11-29 11:41:40 +0200

    Fix 255 exit status

--- desktop/src/com/tgf/twf/desktop/DesktopLauncher.java
+++ desktop/src/com/tgf/twf/desktop/DesktopLauncher.java
@@ -7,6 +7,7 @@ import com.tgf.twf.TheWorldFactoryGame;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.forceExit = false;
 		new LwjglApplication(new TheWorldFactoryGame(), config);
 	}
 }
