73c9bac5a5865e07a69193392e0714c7a18565ae
Fernand Sieber <sieberf@amazon.com>
2019-11-29 11:33:13 +0200

    Hello libgdx

---
+++ desktop/src/com/tgf/twf/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.tgf.twf.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.tgf.twf.TheWorldFactoryGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new TheWorldFactoryGame(), config);
+	}
+}
