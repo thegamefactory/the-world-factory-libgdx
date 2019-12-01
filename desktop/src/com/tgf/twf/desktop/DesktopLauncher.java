c6ce073f9de5c50d1431b103ab99147932eb65fd
Fernand Sieber <sieberf@amazon.com>
2019-12-01 22:51:50 +0200

    Update to lwjgl 3

--- desktop/src/com/tgf/twf/desktop/DesktopLauncher.java
+++ desktop/src/com/tgf/twf/desktop/DesktopLauncher.java
@@ -1,7 +1,7 @@
 package com.tgf.twf.desktop;
 
-import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
-import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 import com.google.inject.Guice;
 import com.tgf.twf.TheWorldFactoryGame;
 import com.tgf.twf.config.WorldModule;
@@ -9,10 +9,9 @@ import com.tgf.twf.core.world.World;
 
 public class DesktopLauncher {
     public static void main(final String[] arg) {
-        final LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-        config.forceExit = false;
+        final Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
 
         final World world = Guice.createInjector(new WorldModule()).getInstance(World.class);
-        new LwjglApplication(new TheWorldFactoryGame(world), config);
+        new Lwjgl3Application(new TheWorldFactoryGame(world), config);
     }
 }
